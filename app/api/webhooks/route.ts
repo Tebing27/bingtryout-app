d598689b2ae5a7c79610d1de511337f4cd9adf6e
Bing <tebingtsaniansyah56@gmail.com>
2024-12-19 00:36:54 +0700

    add mongodb

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createdOrUpdateUser, deleteUser } from '@/lib/actions/user' 
 
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
@@ -52,5 +53,48 @@ export async function POST(req: Request) {
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
-  return new Response('Webhook received', { status: 200 })
+  if(eventType === 'user.created' || eventType === 'user.updated'){
+    const { id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username } = evt?.data
+        try {
+            await createdOrUpdateUser(
+                id,
+                first_name,
+                last_name,
+                image_url,
+                email_addresses,
+                username
+              );
+              return new Response('User is created or updated', {
+                status: 200,
+              });
+            } catch (error) {
+              console.log('Error creating or updating user:', error);
+              return new Response('Error occured', {
+                status: 400,
+              });
+        }
+  }
+  if (eventType === 'user.deleted') {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+      return new Response('User is deleted', {
+        status: 200,
+      });
+    } catch (error) {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', {
+        status: 400,
+      });
+    }
+  }
+
+  return new Response('', { status: 200 });
 }
+
+//BmWAi9WbgNFnuqGG
